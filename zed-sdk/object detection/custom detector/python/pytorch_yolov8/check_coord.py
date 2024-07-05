03443c035de775ad484ed97334a7472b6959f136
Adeirman46 <adeirman387@gmail.com>
2024-07-05 15:39:50 +0700

    add zed-yolo multitask

--- zed-sdk/object detection/custom detector/python/pytorch_yolov8/check_coord.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-# Uncomment the next line for use in a Jupyter notebook
-# This enables the interactive matplotlib window
-# %matplotlib inline
-image = mpimg.imread('/home/irman/Documents/zed-sdk/object detection/custom detector/python/pytorch_yolov8/videos/jalan_tol_new.png')
-plt.imshow(image)
-plt.show() 
